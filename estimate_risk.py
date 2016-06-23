649621a53e9b762a6cba9960563fcae87d4167d1
mdekauwe <mdekauwe@gmail.com>
2016-06-24 01:26:05 +1000

    Cleaned up script

--- estimate_risk.py
+++ estimate_risk.py
@@ -18,7 +18,7 @@ import sys
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.cross_validation import train_test_split
+from patsy import dmatrices
 
 def main():
 
@@ -29,28 +29,33 @@ def main():
     df.hist()
     plt.show()
 
-    X = df[['credit.policy', 'int.rate','installment',\
-            'log.annual.inc','dti','fico','days.with.cr.line',\
-            'revol.bal','revol.util','inq.last.6mths', 'delinq.2yrs','pub.rec']]
-    y = df['not.fully.paid']
+    # clean up the dataframe
+    df.rename(columns={'not.fully.paid': 'not_fully_paid',
+                       'credit.policy': 'credit_policy',
+                       'int.rate': 'int_rate',
+                       'log.annual.inc': 'log_annual_inc',
+                       'days.with.cr.line': 'days_with_cr_line',
+                       'revol.bal': 'revol_bal',
+                       'inq.last.6mths': 'inq_last_6mths',
+                       'delinq.2yrs': 'delinq_2yrs',
+                       'pub.rec': 'pub_rec'}, inplace=True)
+
+    y, X = dmatrices('not_fully_paid ~ credit_policy + int_rate + \
+                     installment + log_annual_inc + dti + \
+                     days_with_cr_line + revol_bal + inq_last_6mths + \
+                     delinq_2yrs + pub_rec',
+                     df, return_type='dataframe')
 
     model = LogisticRegression()
-
-    # split 70% for training and 30% for testing
-    (X_train, X_test,
-     y_train, y_test) = train_test_split(X, y, random_state=144, test_size=0.3)
-
-    model.fit(X_train, y_train)
-    print 'Model accuracy: %f' % (model.score(X_train, y_train) * 100.0)
-    #print "Coefficients:", model.coef_
-    print "Intercept:",  model.intercept_
-
-    names = list(X.columns.values)
-
-    for i,name in enumerate(names):
-        print name, model.coef_[0,i]
-
+    model.fit(X, y)
     predict = model.predict(X)
+
+    print
+    print
+    print 'Model accuracy: %f' % (model.score(X, y) * 100.0)
+    print pd.DataFrame(zip(X.columns, np.transpose(model.coef_)))
+    
+
 
     #plt.plot(X, predict, "ro")
     #plt.plot(X, y, "ko")
